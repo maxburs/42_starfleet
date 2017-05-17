3a0bffacd857d7a3416de1b7456571717e07f68b
maxwell burson <maxwellburson@gmail.com>
2017-05-16 18:25:57 -0700

    finished day2 ex1

---
+++ day_01/ex01/reverse.c
@@ -0,0 +1,21 @@
+#include "header.h"
+#include <string.h> //NULL
+#include <stdio.h> //printf()
+
+static void recursive_step(struct s_node *lst)
+{
+	if (lst->next)
+	{
+		recursive_step(lst->next);
+		fputc(' ', stdout);
+	}
+	fputs(lst->word, stdout);
+}
+
+void printReverse(struct s_node *lst)
+{
+	if (lst == NULL)
+		return ;
+	recursive_step(lst);
+	fputc('\n', stdout);
+}
