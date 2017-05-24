1b84a6962b5206e11200f21b13b75dde59c0641a
maxwell burson <maxwellburson@gmail.com>
2017-05-23 20:57:39 -0700

    finished ex09

---
+++ day04/ex09/header.h
@@ -0,0 +1,25 @@
+#ifndef HEADER_H
+# define HEADER_H
+
+/*--------------------------------
+  !! required structure
+  --------------------------------*/
+
+
+/*--------------------------------
+  :) function you must implement
+  --------------------------------*/
+int occupiedPlaces(unsigned int parkingRow);
+
+/*--------------------------------
+  ?? test function used in main 
+  --------------------------------*/
+
+
+/*--------------------------------
+  &  your own other function
+  --------------------------------*/
+
+int add(int num, int bit);
+
+#endif
---
+++ day04/ex09/occupiedPlaces.c
@@ -0,0 +1,30 @@
+#include "header.h"
+
+#include <stdio.h>
+
+int occupiedPlaces(unsigned int parkingRow)
+{
+	int result = 0;
+
+	if (parkingRow & 0x80000000)
+	{
+		parkingRow &= 0x7FFFFFFF;
+		result = add(result, 1);
+	}
+	while (parkingRow)
+	{
+		if (parkingRow & 1)
+			result = add(result, 1);
+		parkingRow = parkingRow >> 1;
+	}
+	return (result);
+}
+
+int add(int num, int bit)
+{
+	if (num & bit)
+	{
+		return (add(num & ~bit, bit << 1));
+	}
+	return (num | bit);
+}
