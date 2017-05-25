b3d37c34b80be22c37a7aca934f6f60a0e1cf0fb
Maxwell BURSON <maxwellburson@gmail.com>
2017-05-24 17:59:33 -0700

    working on day05/ex02

---
+++ day05/ex02/bestPriceV2.c
@@ -0,0 +1,37 @@
+#include "header.h"
+#include <stdlib.h> //malloc(), free()
+#include <string.h> //NULL
+
+double optimizedBestPrice(int pizzaSize, double *prices) {
+	int *slices;
+	double bestPrice = 0;
+
+	if (NULL == (slices = malloc(sizeof(*slices) * (pizzaSize + 1))))
+		return (-1);
+	recursiveStep(pizzaSize, slices, 0, &bestPrice, prices, pizzaSize);
+	free(slices);
+	return (bestPrice);
+}
+
+void recursiveStep(int sizeLeft, int *slices, int step, double *bestPrice, double *prices, int maxSize) {
+	slices[step] = 0;
+	if (sizeLeft == 0)
+		comparePrices(slices, bestPrice, prices);
+	while (0 < sizeLeft--) {
+		slices[step]++;
+		if (sizeLeft < maxSize * 2)
+			maxSize--;
+		recursiveStep(sizeLeft - maxSize, slices, step + 1, bestPrice, prices);
+	}
+}
+
+void comparePrices(int *slices, double *bestPrice, double *prices) {
+	double price = 0;
+
+	while (*slices) {
+		price += prices[*slices];
+		slices++;
+	}
+	if (*bestPrice < price)
+		*bestPrice = price;
+}
--- day05/ex02/main.c
+++ day05/ex02/main.c
@@ -17,7 +17,7 @@ int main(int ac, char **av)
 	/*-------------------
 	launch your test here
 	--------------------*/
-        // printf("%d : %.2f\n", pizzaSize, optimizedBestPrice(pizzaSize, pricesCont->items));
+        printf("%d : %.2f\n", pizzaSize, optimizedBestPrice(pizzaSize, pricesCont->items));
 
 	return (0);
 }
