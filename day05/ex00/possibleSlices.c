70d6b683d5aa96789521976e5b9e5f4b4ff3ee6e
Maxwell BURSON <maxwellburson@gmail.com>
2017-05-24 17:17:19 -0700

    made possibleslices.c more pretty

--- day05/ex00/possibleSlices.c
+++ day05/ex00/possibleSlices.c
@@ -1,9 +1,8 @@
 #include "header.h"
 #include <stdio.h> //printf()
 #include <string.h> //bzero()
-#include <limits.h>
-#include <stdlib.h>
-#include <stdint.h>
+#include <limits.h> //INT_MAX
+#include <stdlib.h> //malloc(), free()
 
 void printPossibleSlices(int pizzaSize) {
 	int *slices;
@@ -11,14 +10,14 @@ void printPossibleSlices(int pizzaSize) {
 	if (NULL == (slices = malloc(sizeof(*slices) * ((unsigned int)INT_MAX + 1))))
 		return ;
 	recursiveStep(pizzaSize, slices, 0);
+	free(slices);
 }
 
 void recursiveStep(int pizzaSize, int *slices, int step) {
 	slices[step] = 0;
 	if (pizzaSize == 0)
 		printSlices(slices);
-	while (0 < pizzaSize--)
-	{
+	while (0 < pizzaSize--) {
 		slices[step]++;
 		recursiveStep(pizzaSize, slices, step + 1);
 	}
@@ -27,8 +26,7 @@ void recursiveStep(int pizzaSize, int *slices, int step) {
 void printSlices(int *slices)
 {
 	printf("[");
-	while (*(slices + 1))
-	{
+	while (slices[1]) {
 		printf("%d, ", *slices);
 		slices++;
 	}
